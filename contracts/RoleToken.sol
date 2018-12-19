3587c1c516df70a676f84b08ac7df3c9b0df5b67
roleengineer <roleengineer@gmail.com>
2018-12-19 02:02:44 +0200

    3. Delegated Transfer

--- contracts/RoleToken.sol
+++ contracts/RoleToken.sol
@@ -5,7 +5,12 @@ contract RoleToken {
   mapping(address => uint) public balanceOf;
   string public name = 'Role Token';
   string public symbol = 'RT';
+  //allowance
+  mapping(address => mapping(address => uint)) public allowance;
+  //transeffrom event
   event Transfer(address indexed _from, address indexed _to, uint _value);
+
+  event Approval(address indexed _owner, address indexed _spender, uint _value);
 
   constructor (uint _initialSupply) public {
     balanceOf[msg.sender] = _initialSupply;
@@ -17,9 +22,35 @@ contract RoleToken {
   function transfer(address _to, uint _value) public returns(bool success) {
     require(balanceOf[msg.sender] >= _value);
     require(_to != 0x0);
+
     balanceOf[msg.sender] -= _value;
     balanceOf[_to] += _value;
+
     emit Transfer(msg.sender, _to, _value);
+
     return true;
   }
+
+  //approve
+  function approve(address _spender, uint _value) public returns (bool success) {
+    //allowance
+    allowance[msg.sender][_spender] = _value;
+    //approve event
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+  //transeffrom
+  function transferFrom(address _from, address _to, uint _value) public returns (bool success) {
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+    balanceOf[_from] -= _value;
+    balanceOf[_to] += _value;
+    //Update the allowance
+    allowance[_from][msg.sender] -= _value;
+    emit Transfer(_from, _to, _value);
+    return true;
+
+  }
+
+
 }
